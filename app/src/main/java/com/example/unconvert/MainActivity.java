168439addee253d287503fa88ee8efdc1010230e
Shantanu Thakur <146916282+shanu31396@users.noreply.github.com>
2025-02-15 22:40:46 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/unconvert/MainActivity.java
+++ app/src/main/java/com/example/unconvert/MainActivity.java
@@ -25,31 +25,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
         textView = findViewById(R.id.textView2);
         editTextView = findViewById(R.id.editTextView);
 
 //      THIS WAS BY EVENT LISTENER
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @SuppressLint("SetTextI18n")
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-//                String s = editTextView.getText().toString();
-//                double kg = Double.parseDouble(s);
-//                double pound  = kg * 2.20462;
-//                textView.setText("The corresponding value in Pounds is: " + pound + " Pounds.");
-//            }
-//        });
+        button.setOnClickListener(new View.OnClickListener() {
+           @SuppressLint("SetTextI18n")
+           @Override
+           public void onClick(View view) {
+               Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+               String s = editTextView.getText().toString();
+               double kg = Double.parseDouble(s);
+               double pound  = kg * 2.20462;
+               textView.setText("The corresponding value in Pounds is: " + pound + " Pounds.");
+           }
+       });
 
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+    //     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+    //         Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+    //         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+    //         return insets;
+    //     });
+    // }
 
 
 // NOW, MAKING BUTTON RESPONSIVE VIA CALLING A FUNCTION
